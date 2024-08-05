9549b7ab900da6d14b9b985db5210e6de0dbf3e4
CR2429 <WDgaster.gtp@gmail.com>
2024-08-05 04:13:18 -0400

    first commit :)

---
+++ langitia/src/main/java/com/annoapp/langitia/LangitiaApplication.java
@@ -0,0 +1,13 @@
+package com.annoapp.langitia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangitiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangitiaApplication.class, args);
+	}
+
+}
