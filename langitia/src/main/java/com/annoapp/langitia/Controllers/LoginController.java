d9a5b0ddcd6fa5f585caa952dff4056ae304a578
Guillaume Paoli <2142678@cegepoutaouais.qc.ca>
2024-09-19 10:32:08 -0400

    avancement dans le login

--- langitia/src/main/java/com/annoapp/langitia/Controllers/LoginController.java
+++ langitia/src/main/java/com/annoapp/langitia/Controllers/LoginController.java
@@ -1,14 +1,29 @@
 package com.annoapp.langitia.Controllers;
 
+import java.net.Authenticator;
+import java.sql.Savepoint;
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import com.annoapp.langitia.Authentification.MyAuthenticationProvider;
 import com.annoapp.langitia.Authentification.MyUserManager;
 import com.annoapp.langitia.Data.LoginDbContext;
+import com.annoapp.langitia.Models.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -27,10 +42,45 @@ public class LoginController {
     LoginDbContext loginDbContext;
 
     //methode
-    @GetMapping("/")
+    @GetMapping("/login")
     public ModelAndView getMethodName(@RequestParam(value="loginError",required=false,defaultValue="false") String loginError) {
         ModelAndView mv = new ModelAndView("login");
         return mv;
+    }
+
+    @PostMapping("/login")
+    public String login(HttpServletRequest request, HttpServletResponse response) {
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+
+        try {
+            //recuperer les info du formulaire de login
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+
+            //tenter de creer le user
+            User user = new User(username,password,new ArrayList<>());
+            Authentication token = new UsernamePasswordAuthenticationToken(username,password,new ArrayList<>());
+
+            //Valider le token
+            Authentication authentication = myAuthenticationProvider.authenticate(token);
+            securityContext.setAuthentication(authentication);
+            securityContextRepository.saveContext(securityContext, request, response);
+
+            //redirection
+            SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
+            if (savedRequest == null)
+                return "redirect:/";
+            else
+                return "redirect:/"+savedRequest.getRedirectUrl();
+        } catch (AuthenticationException ex) {
+            HttpSession session=request.getSession(true);
+            session.setAttribute("LoginErrorMessage", ex.getMessage());
+            return "redirect:/login/login?loginError=true";
+        } catch(Exception ex){
+            HttpSession session=request.getSession(true);
+            session.setAttribute("LoginErrorMessage", ex.getMessage());
+            return "redirect:/login/login?loginError=true";
+        }
     }
     
 }
--- langitia/src/main/resources/templates/login.jsp
+++ langitia/src/main/resources/templates/login.jsp
@@ -14,9 +14,36 @@
 
         <div class="border border-white shadow-lg p-3 rounded text-white" id="body-div">
 
-            <!--Mettre son code ici-->
-            
-            <!-- :) -->
+            <div class="border">
+                <c:if test="${loginError.equals('true')}">
+                    <div class="alert alert-danger" role="alert">
+                        Erreur des informations d'identification. Essayez encore.<br/>
+                        Cause : ${LoginErrorMessage}
+                    </div>
+                </c:if>
+                <h3>Connexion avec le nom d'usager et le mot de passe</h3>
+                <form name='loginForm' method='post' action="<c:url value='/login/loginpost'/>">
+
+                    <div class="container">
+                        <div class="row m-1">
+                            <div class="col text-end pe-1">Le nom de l'usager:</div>
+                            <div class="col ps-1"><input type='text' name='username' required></div>
+                        </div>
+                        <div class="row m-1">
+                            <div class="col text-end pe-1">Le mot de passe:</div>
+                            <div class="col ps-1"><input type='password' name='password' required/></div>
+                        </div>
+                        <div class="row m-1">
+                            <div class="col"></div>
+                            <div class="col-md-auto">
+                                <input type="submit"/>
+                                <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
+                            </div>
+                            <div class="col"></div>
+                        </div>
+                    </div>
+                </form>
+            </div>
 
         </div>        
     </div>
